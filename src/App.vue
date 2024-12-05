609685e57f494ba197f9de461da82805fc13d3cd
hujunnj <hujuntulin@163.com>
2024-12-05 23:53:42 +0800

    vue-demo 第一次提交

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import {ElContainer, ElAside, ElHeader, ElMain} from 'element-plus'
+import SideComponed from '@/components/SideComponed.vue'
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <SideComponed></SideComponed>
+        </el-aside>
+        <el-main><RouterView/></el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style>
+</style>
